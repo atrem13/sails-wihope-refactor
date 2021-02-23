77d7e790fd0e69512c81d30683a9fc2fa3e0d00f
atrem13 <mertayoga17.my@gmail.com>
2021-02-23 19:55:32 +0800

    controller rapot done

--- api/controllers/RapotController.js
+++ api/controllers/RapotController.js
@@ -6,7 +6,67 @@
  */
 
 module.exports = {
-  
+
+  async create(req, res){
+    try{
+      let params = req.allParams();
+      let attributes = {};
+      if(params.nama_rapot){
+        attributes.nama_rapot = params.nama_rapot;
+        attributes.kode_rapot = attributes.nama_rapot.split(' ').map(el=> el[0]).join('');
+        let result = await Rapot.create(attributes).fetch();
+        return res.json({status: 200, data: result});
+      }else{
+        return res.json({status: 400, msg: 'Input Kosong'});
+      }
+    }catch(e){
+      return res.serverError(e);
+    }
+  },
+
+  async find(req, res){
+    try{
+      let result = await Rapot.find();
+      return result.length ? res.json({status: 200, data: result}) : res.json({status: 404, msg: 'Tabel Kosong'});
+    }catch(e){
+      return res.serverError(e);
+    }
+  },
+
+  async findOne(req, res){
+    try{
+      let params = req.allParams();
+      let result = await Rapot.findOne({id: params.id});
+      return result ? res.json({status: 200, data: result}) : res.json({status: 404, msg: 'Data Tidak Ditemukan'});
+    }catch(e){
+      return res.serverError(e);
+    }
+  },
+
+  async update(req, res){
+    try{
+      let params = req.allParams();
+      let attributes = {updatedAt: Date.now(),};
+      if(params.nama_rapot){
+        attributes.nama_rapot = params.nama_rapot;
+        attributes.kode_rapot = attributes.nama_rapot.split(' ').map(el => el[0]).join('');
+        let result = await Rapot.updateOne({id: params.id}).set(attributes);
+        return result ? res.json({status: 200, data: result}) : res.json({status: 400, msg: 'Update Gagal'});
+      }
+    }catch(e){
+      return res.serverError(e);
+    }
+  },
+
+  async destroy(req, res){
+    try{
+      let params = req.allParams();
+      let result = await Rapot.destroyOne({id: params.id});
+      return result ? res.json({status: 200, data: result}) : res.json({status: 400, msg: 'Delete Gagal'});
+    }catch(e){
+      return res.serverError(e);
+    }
+  }
 
 };
 
